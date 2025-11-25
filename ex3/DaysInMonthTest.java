031c9faf27c72cc29da8ee2d761ac24562a708d6
Torkel1337 <55099372+hanabe0729@users.noreply.github.com>
2025-11-25 15:19:16 +0100

    ex1, ex3 - klar

--- ex3/DaysInMonthTest.java
+++ ex3/DaysInMonthTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class daysInMonthTest {
+class DaysInMonthTest {
 
     @Test
     void daysInMonth1() {
